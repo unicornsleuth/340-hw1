a430302c60af18e1c73cdf69e9db9a2d9f3fd86b
Larissa <lyaholkovsky@gmail.com>
2020-06-01 02:38:42 -0700

    added spinner testing

--- app/src/androidTest/java/com/example/lyaho340hw1/SettingsFragmentTest.java
+++ app/src/androidTest/java/com/example/lyaho340hw1/SettingsFragmentTest.java
@@ -1,5 +1,7 @@
 package com.example.lyaho340hw1;
 
+import android.content.Context;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -8,12 +10,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class SettingsFragmentTest {
@@ -43,15 +52,30 @@ public class SettingsFragmentTest {
 
     @Test
     public void spinnerChange() {
+        onView(withId(R.id.editText_max_distance)).perform(click()).perform(typeText("42"));
+        // scroll to minAge, input
+        onView(withId(R.id.editText_min_age)).perform(scrollTo())
+                .perform(click()).perform(typeText("25"));
+        // maxAge, input
+        onView(withId(R.id.editText_max_age)).perform(scrollTo())
+                .perform(click()).perform(typeText("45"));
+        
+        Context context = activityTestRule.getActivity().getApplicationContext();
         // scroll to gender, switch opt 2
+        String[] genders = context.getResources().getStringArray(R.array.genders);
+        onView(withId(R.id.spinner_gender)).perform(scrollTo()).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(genders[1]))).perform(click());
+        onView(withId(R.id.spinner_gender)).check(matches(withSpinnerText(containsString(genders[1]))));
         // scroll to lookingForGender, switch opt 2
+        onView(withId(R.id.spinner_looking_for_gender)).perform(scrollTo()).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(genders[1]))).perform(click());
+        onView(withId(R.id.spinner_looking_for_gender)).check(matches(withSpinnerText(containsString(genders[1]))));
         // scroll to privacy, switch opt 2
+        String[] privacy = context.getResources().getStringArray(R.array.account_privacy);
+        onView(withId(R.id.spinner_account_privacy)).perform(scrollTo()).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(privacy[1]))).perform(click());
+        onView(withId(R.id.spinner_account_privacy)).check(matches(withSpinnerText(containsString(privacy[1]))));
         // scroll to save button, click
-        // scroll to gender, switch opt 3
-        // scroll to lookingForGender, switch opt 3
-        // scroll to save button, click
-        // scroll to gender, switch opt 4
-        // scroll to lookingForGender, switch opt 4
-        // scroll to save button, click
+        onView(withId(R.id.button_save_settings)).perform(scrollTo()).perform(click());
     }
 }
